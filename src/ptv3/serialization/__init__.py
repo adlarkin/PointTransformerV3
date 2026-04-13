14bd83cb85cdf1f605230ad0eb20eca3cffbe4d8
Ashton Larkin <42042756+adlarkin@users.noreply.github.com>
2026-04-13 15:28:01 -0600

    make this locally installable by pip (need to sort out dependencies)

---
+++ src/ptv3/serialization/__init__.py
@@ -0,0 +1,17 @@
+from .default import (
+    decode,
+    encode,
+    z_order_decode,
+    z_order_encode,
+    hilbert_decode,
+    hilbert_encode,
+)
+
+__all__ = (
+    "encode",
+    "decode",
+    "z_order_encode",
+    "z_order_decode",
+    "hilbert_encode",
+    "hilbert_decode",
+)
