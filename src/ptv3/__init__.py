afb437baaa9d96298f11a5f829c649bbe59416e6
Ashton Larkin <42042756+adlarkin@users.noreply.github.com>
2026-06-01 14:53:50 -0600

    pip installable (need to flesh out pyproject.toml)

---
+++ src/ptv3/__init__.py
@@ -0,0 +1,21 @@
+from .serialization import (
+    decode,
+    encode,
+    hilbert_decode,
+    hilbert_encode,
+    z_order_decode,
+    z_order_encode,
+)
+from .model import (
+    PointTransformerV3,
+)
+
+__all__ = (
+    "decode",
+    "encode",
+    "hilbert_decode",
+    "hilbert_encode",
+    "PointTransformerV3",
+    "z_order_decode",
+    "z_order_encode",
+)
